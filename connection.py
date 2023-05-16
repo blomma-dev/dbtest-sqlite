f909e0ac9dff15427aeeae8d3d8b0db340c2cfa0
Joel B <blomsterjoel@gmail.com>
2023-05-16 13:32:27 +0300

    added function to read users from db, minor fixes
    Co-authored-by: Konsta Martikainen <Chonumahn@users.noreply.github.com>

--- connection.py
+++ connection.py
@@ -11,7 +11,10 @@ def Add_user(cursor, connection):
     sql = "SELECT id, name FROM users"
     cursor.execute(sql)
     result = cursor.fetchall()
-    i = result[-1][0] + 1
+    if not result:
+        i = 1
+    else:
+        i = result[-1][0] + 1
 
     username_exists = any(user_name == row[1] for row in result)
 
@@ -21,14 +24,23 @@ def Add_user(cursor, connection):
     else:    
         sql = "INSERT INTO users (id, name, type) VALUES (?, ?, ?)"
         cursor.execute(sql, (i, user_name, account_type))
-
-        cursor.close()
         connection.commit()
+
 
         print("Record inserted successfully into database.\n")
 
     return
 
+def Read_users(cursor):
+    sql = "SELECT id, name, type FROM users"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print("Users:")
+    for row in result:
+        print("-" + row[1] + ":" + row[2])
+    print("\n")
+    return
+    
 
 def Main():
     try:
@@ -37,13 +49,17 @@ def Main():
         print('Connection to SQLite DB successful\n')
 
         while True:
-            command = input("What do you want to do?\nAdd\nQuit\n\n")
+            command = input("What do you want to do?\nAdd:Read:Quit\n\n").lower()
             
-            if(command == "Add"):
+            if(command == "add"):
                 Add_user(cursor, connection)
+            
+            elif(command == "read"):
+                Read_users(cursor)
                 
-            elif(command == "Quit"):
+            elif(command == "quit"):
                 print("Bye!")
+                cursor.close()
                 break
 
             else:
@@ -51,7 +67,7 @@ def Main():
         
         
     except Exception as e:
-        print("An error occurred, and it is your fault.", e)
+        print("An error occurred, and it is your fault. The error message is: ", e)
         if connection:
             connection.close()
 
